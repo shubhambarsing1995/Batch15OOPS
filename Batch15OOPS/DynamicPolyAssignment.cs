0b009e0a02c046e9f294ce56f0105929909c7675
shubhambarsing1995 <shubhambarsing.sb@gmail.com>
2025-04-15 09:24:13 -0700

    StaticPloyAssignment

---
+++ Batch15OOPS/DynamicPolyAssignment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPS
+{
+    //base/parrent class
+    class Vehicle
+    {
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("vechile engine started");
+        }
+    }
+    //child class
+    class Car: Vehicle
+    {
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("car engine started");
+        }
+    }
+
+    class Truck : Vehicle
+    {
+
+        public override void StartEngine()
+        {
+            Console.WriteLine("Truck engine started");
+        }
+    }
+    class DynamicPolyAssignment
+    {
+        public void Main()
+        {
+            Vehicle vechile1 = new Vehicle();
+            Vehicle vechile2 = new Car();
+            Vehicle vechile3 = new Truck();
+            //Calling method
+            vechile1.StartEngine();
+            vechile2.StartEngine();
+            vechile3.StartEngine();
+        }
+
+    }
+}
---
+++ Batch15OOPS/StaticPloyAssignment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPS
+{  class Calculator
+    {
+        public double Calculation(int a, double b)
+        {
+            return a + b;
+        }
+        public double Calculation(double a, int b)
+        {
+            return a - b;
+        }
+        public double Calculation(double a, double b)
+        {
+            return a * b;
+        }
+        public double Calculation(int a, int b)
+        {
+            return a / b;
+        }
+    }
+    class StaticPloyAssignment
+    {
+        static void Main()
+        {
+            Calculator calci = new Calculator();
+            double sum = calci.Calculation(7,9.5);
+            double subtract = calci.Calculation(7.8,5);
+            double multiply = calci.Calculation(7.5,9.5);
+            double division = calci.Calculation(7,9);
+
+
+            Console.WriteLine($"sum of integer value:{sum}");
+            Console.WriteLine($"subtraction of integer value:{subtract}");
+            Console.WriteLine($"multiplication of integer value:{multiply}");
+            Console.WriteLine($"division of integer value:{division}");
+
+
+
+        }
+
+
+    }
+}
